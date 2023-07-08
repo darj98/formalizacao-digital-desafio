38fbcab414ba4714523eb59dcbe2a75968858adb
Daniel de Azevedo <daniel.arj.profissional@gmail.com>
2023-07-08 16:34:33 -0300

    Commiting initial structure

---
+++ src/main/java/com/formalizacao/cartao/FormalizacaoCartaoCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.formalizacao.cartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormalizacaoCartaoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormalizacaoCartaoCreditoApplication.class, args);
+	}
+
+}
